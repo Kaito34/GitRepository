3a4fe2a790d33975b2dd0e5aabdb0dbe1c6e195a
楠本海斗 <kusumotokaito@kusumotokaitos-MacBook-Pro.local>
2020-04-22 18:15:22 +0900

    BattleFlowクラスを導入。固まった時の対処法を加えて、フレンド選択のフローを作成

--- object-oriented.py
+++ object-oriented.py
@@ -50,10 +50,10 @@ class Image_recognition:
     click: 画像の座標をクリックする
     """
     def __init__(self,filename):
-        self.__filename = filename
+        self.filename = filename
 
     def judge(self):
-        if pg.locateCenterOnScreen(self.__filename,confidence=0.8,region=regionbox):
+        if pg.locateCenterOnScreen(self.filename,confidence=0.8,region=regionbox):
             return True
         else:
             return False
@@ -62,7 +62,7 @@ class Image_recognition:
         # openCVを用いて範囲内に画像があるか調べる
         #文字認識も有効
         try:
-            pos_x,pos_y = pg.locateCenterOnScreen(self.__filename,confidence=0.8,region=regionbox)
+            pos_x,pos_y = pg.locateCenterOnScreen(self.filename,confidence=0.8,region=regionbox)
         except:
             pos_x,pos_y = (None,None)
         #現在地によって機能を変える(未実装)
@@ -71,9 +71,10 @@ class Image_recognition:
     @property
     def click(self):
         try:
-            self.__pos_x,self.__pos_y =  Image_recognition(self.__filename).pos()
-            print(self.__pos_x,self.__pos_y)
-            pg.click(self.__pos_x,self.__pos_y)
+            #まずはクリックにトライ
+            self.pos_x,self.pos_y =  Image_recognition(self.filename).pos()
+            print(self.pos_x,self.pos_y)
+            pg.click(self.pos_x,self.pos_y)
             return True
         except:
             return False
@@ -91,31 +92,57 @@ class Where:
 
 
 """フローの実行クラス"""
-class BattleFlow:
-    def __init__(self,friend_summon):
-        self.friend_summon = friend_summon
+class BattleFlow():
+    def __init__(self,summon_friend='summon_friend.png'):
+        l = [[] for i in range(11)]
+        l[0] = self.summon_friend = Image_recognition(summon_friend)
+        l[1] = self.ok = Image_recognition("ok.png")
+        l[2] = self.reload = Image_recognition("reload.png")
+        l[3] = self.bookmark = Image_recognition("bookmark_win.png")
+        l[4] = self.quest_supporter = Image_recognition("quest_supporter_win.png")
+        l[5] = self.raid_multi = Image_recognition("raid_multi_win.png")
+        l[6] = self.raid = Image_recognition("raid_win.png")
+        l[7] = self.result_multi = Image_recognition("result_multi_win.png")
+        l[8] = self.result = Image_recognition("result_win.png")
+        l[9] = self.quest_supporter = Image_recognition("result_multi_win.png")
+        l[10] = self.quest_supporter = Image_recognition("result_multi_win.png")
 
-    """フレンド選択までのフロー"""
+        self.prepare()
+
+    def prepare(self):
+        for i in range(11)
+            if not l[i].judge():
+                print(l[i].filename+"does not exist.")
+                sys.exit()
+
+    """固まった時の対処"""
+    def if_move(self,curlist,duration=0,url):
+        for num in range(len(curlist)-1):
+            try:
+                curlist[num].click
+            except:
+                time.sleep(duration)
+                if curlist[num+1].judge():
+                    pass
+                elif not curlist[num+1].judge(): #curlist[num+1]がない時
+                    time.sleep(5)
+                    if curlist[num+1].judge():
+                        pass
+                    else:
+                        if_move([self.reload,self.bookmark,url],0.5)
+                        return if_move(self,curlist,duration=0,url)
 
     """フレンド選択からバトルスタートのフロー"""
+    @property
     def friend_select(self):
-        sf =0
+        return if_move([self.summon_friend,self.ok,self.raid_multi],1,self.quest_supporter)
+
 
 
 if __name__ == "__main__":
 
-    summon_friend = Image_recognition("summon_friend.png")
-    bookmark = Image_recognition("bookmark.png")
-    varuna = Image_recognition("varuna.png")
-    ok = Image_recognition("ok.png")
-
-    print(ok.pos)
-
-    if not summon_friend.judge(): #quest_supporterがない時
-        bookmark.click #bookmarkに移動　#固まった時の対応
-    print("sdf")
-    summon_friend.click #フレ選択
-    ok.click
+    BattleFlow('summon_friend.png').friend_select
+    #BattleFlow('summon_friend.png').battle
 
     """
     #理想
